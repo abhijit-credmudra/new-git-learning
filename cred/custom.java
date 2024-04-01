1a2896297eaf9de3f114cb32b531cbaad66408bb
abhijit-credmudra <abhijit.kumar@credmudra.com>
2024-04-01 14:53:03 +0530

    updated

--- cred/custom.java
+++ cred/custom.java
@@ -4,5 +4,7 @@ public class custom {
     public static void main(String[] args) {
         System.out.println("h");
         System.out.println("i");
+        System.out.println("j");
+        System.out.println("k");
     }
 }
