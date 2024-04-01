5a0dccf69eb36f752020039d59298b20a07b25ba
abhijit-credmudra <abhijit.kumar@credmudra.com>
2024-04-01 13:16:12 +0530

    testing

---
+++ Testing.java
@@ -0,0 +1,9 @@
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println("executed");
+          System.out.println("pulling out new changes");
+        System.out.println("changed");
+        System.out.println("executing multiple push");
+        System.out.println("executing  push");
+    }
+}
